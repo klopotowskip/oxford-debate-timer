7375acacaa80ef26e43bf4f7dd2530d125f54bf5
Piotr Kłopotowski <pietrek777@gmail.com>
2018-11-15 21:21:04 +0100

    Remove bullets from radio buttons

--- src/components/scene/setup/SetupForm.js
+++ src/components/scene/setup/SetupForm.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 import { getSetupMessages } from '~/src/locale/locale-supplier';
 
-const messages = getSetupMessages(); 
+const messages = getSetupMessages();
 
 export default class SetupForm extends React.Component {
 
@@ -28,19 +28,15 @@ export default class SetupForm extends React.Component {
           <div className="lduration-radio">
             <span className="lduration-radio__title">{messages.LONG_SPEECH_DURATION}</span>
             <div>
-              <input type="radio" id="lduration-debug" name="lduration" value="50" defaultChecked/>
-              <label htmlFor="lduration-debug">2 min</label>
-            </div>
-            <div>
-              <input type="radio" id="lduration-3min" name="lduration" value="1800"/>
+              <input className="setup-form__radio-button" type="radio" id="lduration-3min" name="lduration" value="1800" />
               <label htmlFor="lduration-3min">3 min</label>
             </div>
             <div>
-              <input type="radio" id="lduration-4min" name="lduration" value="2400"/>
+              <input className="setup-form__radio-button" type="radio" id="lduration-4min" name="lduration" value="2400" defaultChecked />
               <label htmlFor="lduration-4min">4 min</label>
             </div>
             <div>
-              <input type="radio" id="lduration-5min" name="lduration" value="3000"/>
+              <input className="setup-form__radio-button" type="radio" id="lduration-5min" name="lduration" value="3000"/>
               <label htmlFor="lduration-5min">5 min</label>
             </div>
           </div>
@@ -48,34 +44,30 @@ export default class SetupForm extends React.Component {
           <div className="squantity-radio">
             <span className="squantity-radio__title">{messages.SHORT_SPEECH_QUANTITY}</span>
             <div>
-              <input type="radio" id="squantity-0" name="squantity" value="0"/>
+              <input className="setup-form__radio-button" type="radio" id="squantity-0" name="squantity" value="0" />
               <label htmlFor="squantity-0">0</label>
             </div>
             <div>
-              <input type="radio" id="squantity-1" name="squantity" value="1"/>
+              <input className="setup-form__radio-button" type="radio" id="squantity-1" name="squantity" value="1" />
               <label htmlFor="squantity-1">1</label>
             </div>
             <div>
-              <input type="radio" id="squantity-2" name="squantity" value="2" defaultChecked/>
+              <input className="setup-form__radio-button" type="radio" id="squantity-2" name="squantity" value="2" defaultChecked />
               <label htmlFor="squantity-2">2</label>
             </div>
           </div>
           <div className="sduration-radio">
             <span className="sduration-radio__title">{messages.SHORT_SPEECH_DURATION}</span>
             <div>
-              <input type="radio" id="sduration-debug" name="sduration" value="30" defaultChecked/>
-              <label htmlFor="sduration-3s">DEBUG</label>
-            </div>
-            <div>
-              <input type="radio" id="sduration-30s" name="sduration" value="300"/>
+              <input className="setup-form__radio-button" type="radio" id="sduration-30s" name="sduration" value="300" defaultChecked />
               <label htmlFor="sduration-30s">30 s</label>
             </div>
             <div>
-              <input type="radio" id="sduration-1min" name="sduration" value="600"/>
+              <input className="setup-form__radio-button" type="radio" id="sduration-1min" name="sduration" value="600" />
               <label htmlFor="sduration-1min">1 min</label>
             </div>
             <div>
-              <input type="radio" id="sduration-2min" name="sduration" value="1200"/>
+              <input className="setup-form__radio-button" type="radio" id="sduration-2min" name="sduration" value="1200" />
               <label htmlFor="sduration-2min">2 min</label>
             </div>
 
