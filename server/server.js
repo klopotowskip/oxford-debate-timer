52c2c2157191269aaf24361f01ba5e2ba53d87d0
Piotr Kłopotowski <pietrek777@gmail.com>
2018-11-21 22:58:22 +0100

    Prepare for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 
-const port = 3000;
 app.listen(port, () => {
   console.log("Starting production server on port", port);
 });
